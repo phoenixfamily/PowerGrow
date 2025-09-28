ea787308049293bacc007633cc92f92de70bd7d5
abbaslotfinasab <abbas.lotfinasab2025@gmail.com>
2025-09-28 23:32:30 +0330

    today filter added

--- Product/views.py
+++ Product/views.py
@@ -1093,7 +1093,8 @@ class TodayParticipantsView(ListAPIView):
 
     def get_queryset(self):
         pk = self.kwargs['pk']
-        today_index = jdatetime.date.today().weekday()
+        # today_index = jdatetime.date.today().weekday()
+        today_index = 0
         return Participants.objects.filter(
             course_id=pk,
             expired=False,
